3ab37f42d3ccdbd8fd409c85f5a034bcb383fa5e
A E <abel7185@gmail.com>
2026-06-26 02:09:12 +0000

    File changes

--- src/components/research/ResearchLayout.jsx
+++ src/components/research/ResearchLayout.jsx
@@ -11,7 +11,7 @@ export default function ResearchLayout() {
   return (
     <div className="min-h-screen bg-[#0A0E17] text-slate-100">
       <DarkNavBar />
-      <main>
+      <main className="min-h-[calc(100vh-4rem)]">
         <Outlet />
       </main>
     </div>
