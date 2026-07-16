ed7cfd6ec76b403c8de2a223126925ca8c214138
A E <abel7185@gmail.com>
2026-07-16 00:21:08 +0000

    File changes

--- src/functions/suttainNutrition.js
+++ src/functions/suttainNutrition.js
@@ -1,5 +1,6 @@
 import { base44 } from '@/api/base44Client';
 
 export async function suttainNutrition(payload) {
-  return await base44.functions.invoke('suttainNutrition', payload);
+  const res = await base44.functions.invoke('suttainNutrition', payload);
+  return res.data;
 }
